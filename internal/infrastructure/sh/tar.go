6e39afd52bf7999038677a921f4dfd214803c47e
artarts36 <temicska99@mail.ru>
2024-02-16 22:08:37 +0300

    chore: catch tar error

--- internal/infrastructure/sh/tar.go
+++ internal/infrastructure/sh/tar.go
@@ -1,8 +1,11 @@
 package sh
 
 import (
+	"bytes"
 	"context"
+	"errors"
 	"os/exec"
+	"strings"
 )
 
 type Tar struct {
@@ -13,5 +16,17 @@ type TarExtractParams struct {
 }
 
 func (t *Tar) Extract(ctx context.Context, params *TarExtractParams) ([]byte, error) {
-	return exec.CommandContext(ctx, "tar", "-xf", params.ArchivePath).Output()
+	var errBuffer bytes.Buffer
+
+	cmd := exec.CommandContext(ctx, "tar", "-xf", params.ArchivePath)
+	cmd.Stderr = &errBuffer
+	res, err := cmd.Output()
+	if err != nil {
+		err = errors.New(strings.Join([]string{
+			err.Error(),
+			errBuffer.String(),
+		}, ": "))
+	}
+
+	return res, err
 }
